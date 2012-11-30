0f151d59baf2aeb301b1da73ef4cb2e3cf2f0158
Nicklas Nyegaard <nnyegaard@gmail.com>
2012-11-30 10:35:24 +0100

    Start: ColorPicker Feature

--- src/src/me/nyegaard/AAoA/NotificationView.java
+++ src/src/me/nyegaard/AAoA/NotificationView.java
@@ -6,13 +6,20 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
+import com.samsung.spen.settings.SettingStrokeInfo;
 import com.samsung.spensdk.SCanvasConstants;
 import com.samsung.spensdk.SCanvasView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 /**
  * Project: AAoA
@@ -31,18 +38,25 @@ public class NotificationView extends Activity
     private SCanvasView		mSCanvas;
     private ImageView       mPenBtn;
     private ImageView		mEraserBtn;
+    private ImageView       mSaveBtn;
+    private ImageView		mColorPickerBtn;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.canvas);
+        mContext = this;
+
+        //------------------------------------
+        // UI Settings
+        //------------------------------------
+        mPenBtn = (ImageView) findViewById(R.id.penBtn);
+
 
         //------------------------------------
         // Create SCanvasView
         //------------------------------------
-
-        mContext = this;
         mCanvasContainer = (FrameLayout) findViewById(R.id.canvas);
         mSCanvas = new SCanvasView(mContext);
         mCanvasContainer.addView(mSCanvas);
@@ -50,7 +64,7 @@ public class NotificationView extends Activity
         //------------------------------------
         // Create Button listeners
         //------------------------------------
-        mPenBtn = (ImageView) findViewById(R.id.penBtn);
+
         mPenBtn.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -72,9 +86,44 @@ public class NotificationView extends Activity
             }
         });
 
+        mSaveBtn = (ImageView) findViewById(R.id.saveBtn);
+        mSaveBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                saveCanvas();
+                Toast.makeText(getBaseContext(), "Saving", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
         nm.cancel(getIntent().getExtras().getInt("notificationID"));
+    }
+
+    public void saveCanvas()
+    {
+        mSCanvas.setDrawingCacheEnabled(true);
+
+        Bitmap bitmap = mSCanvas.getBitmap(true);
+
+        String extr = Environment.getExternalStorageDirectory().toString();
+        File myPath = new File(extr, "test.jpg");
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(myPath);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            fos.flush();
+            fos.close();
+        }catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        finish();
     }
 
     @Override
